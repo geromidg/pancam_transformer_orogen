ac1755109ecd183af95508f4b1e7aa6455adeed8
Dimitris Geromichalos <geromidg@gmail.com>
2018-01-04 20:19:47 +0100

    Cleanup auto-generated code

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -1,54 +1,10 @@
-/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
-
 #include "Task.hpp"
 
-using namespace pancam_transformer;
+namespace pancam_transformer {
 
-Task::Task(std::string const& name)
-    : TaskBase(name)
-{
-}
-
-Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
-    : TaskBase(name, engine)
-{
-}
-
-Task::~Task()
-{
-}
-
-
-
-/// The following lines are template definitions for the various state machine
-// hooks defined by Orocos::RTT. See Task.hpp for more detailed
-// documentation about them.
-
-bool Task::configureHook()
-{
-    if (! TaskBase::configureHook())
-        return false;
-    return true;
-}
-bool Task::startHook()
-{
-    if (! TaskBase::startHook())
-        return false;
-    return true;
-}
-void Task::updateHook()
-{
+void Task::updateHook(void) {
     TaskBase::updateHook();
 }
-void Task::errorHook()
-{
-    TaskBase::errorHook();
-}
-void Task::stopHook()
-{
-    TaskBase::stopHook();
-}
-void Task::cleanupHook()
-{
-    TaskBase::cleanupHook();
-}
+
+}  // namespace pancam_transformer
+
